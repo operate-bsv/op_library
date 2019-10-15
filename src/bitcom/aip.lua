6433edd60a5e44bf50f3c7e2d93eee608c014e5a
libitx <libitx@protonmail.com>
2019-10-15 23:51:16 +0100

    Implemented AIP protocol function.

--- src/bitcom/aip.lua
+++ src/bitcom/aip.lua
@@ -1,29 +1,91 @@
 --[[
 Implements the [Author Idendity Protocol](https://github.com/BitcoinFiles/AUTHOR_IDENTITY_PROTOCOL).
 
-**DRAFT**
+An `_AIP` attribute is added to the state containing a table of AIP signatures,
+allowing the protocol to be used multiple times in the same tape. Each AIP object
+contains the protocol paramaters, plus a `verified` boolean attribute.
 
-@version 0.0.2
+## Examples
+
+    OP_FALSE OP_RETURN
+      "19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut"
+        "Hello world!"
+        "text/plain"
+        "utf8"
+        0
+        |
+      $REF
+        "BITCOIN_ECDSA"
+        "1EXhSbGFiEAZCE5eeBvUxT6cBVHhrpPWXz"
+        "HKzuHb43Xj4XpmK1YJROD/eN/58ZR0T7LuRi2QW8eFcnQg1d7tSy3QGQI/VQr09PeTQFAXniFyIFkqQYgvAlHvQ="
+    # {
+    #   _AIP: [
+    #     {
+    #       algo: "BITCOIN_ECDSA",
+    #       address: "1EXhSbGFiEAZCE5eeBvUxT6cBVHhrpPWXz",
+    #       indices: [],
+    #       signature: "HKzuHb43Xj4XpmK1YJROD/eN/58ZR0T7LuRi2QW8eFcnQg1d7tSy3QGQI/VQr09PeTQFAXniFyIFkqQYgvAlHvQ=",
+    #       verified: true
+    #     }
+    #   ]
+    # }
+
+@version 0.1.0
 @author Libs
 ]]--
 return function(state, algo, address, signature, ...)
   state = state or {}
+  local algo = string.upper(algo or '')
+  local indices = {}
+  local message = ''
+
+  assert(
+    type(state) == 'table',
+    'Invalid state type.')
+  assert(
+    algo == 'BITCOIN_ECDSA',
+    'Invalid signature algorithm. Must be BITCOIN_ECDSA.')
 
   -- Define AIP object
   local aip = {
     algo = algo,
     address = address,
     signature = signature,
-    indices = {...}
+    verified = false
   }
 
-  -- Attach veryify method
-  function aip.verify(pubKey)
-    -- Not implemented yet
-    return nil
+  -- If no indices provided, assume all fields prior to current global index
+  -- Otherwise, get fields by index as specified
+  if next({...}) == nil then
+    local max = ctx.global_index or 0
+    for idx = 0, max-1 do
+      table.insert(indices, idx)
+    end
+    aip.indices = {}
+  else
+    for k, idx in ipairs({...}) do
+      -- Indices can be encoded as strings or unsigned integers
+      if string.match(idx, '^%d+$')
+        then idx = tonumber(idx)
+        else idx = string.byte(idx)
+      end
+      table.insert(indices, idx)
+    end
+    aip.indices = indices
   end
 
-  state['_AIP'] = aip
+  -- Get tape data, then iterate over indeces to build message for verification
+  local tape = ctx.get_tape()
+  if tape ~= nil then
+    for k, idx in ipairs(indices) do
+      message = message .. tape[idx + 1]
+    end
+    aip.verified = crypto.bitcoin_message.verify(signature, message, address, {encoding = 'binary'})
+  end
+
+  -- Add signature to table, allowing multiple signature adding to same state
+  state['_AIP'] = state['_AIP'] or {}
+  table.insert(state['_AIP'], aip)
 
   return state
 end
--- test/bitcom/aip_test.exs
+++ test/bitcom/aip_test.exs
@@ -8,21 +8,88 @@ defmodule Bitcom.AIPTest do
     }
   end
 
-  describe "SET without a context" do
-    test "must set simple key values", ctx do
+
+  describe "simple examples" do
+    test "must set correct attributes", ctx do
       res = %FBAgent.Cell{script: ctx.script, params: ["BITCOIN_ECDSA", "1AKHViYgBGbmxi8qiJkNvoHNeDu9m3MfPE", "##computed_sig##"]}
       |> FBAgent.Cell.exec!(ctx.vm)
-      assert res["_AIP"]["algo"] == "BITCOIN_ECDSA"
-      assert res["_AIP"]["address"] == "1AKHViYgBGbmxi8qiJkNvoHNeDu9m3MfPE"
-      assert res["_AIP"]["signature"] =="##computed_sig##"
-      assert is_function(res["_AIP"]["verify"])
+      |> Map.get("_AIP")
+      |> List.first
+
+      assert res["algo"] == "BITCOIN_ECDSA"
+      assert res["address"] == "1AKHViYgBGbmxi8qiJkNvoHNeDu9m3MfPE"
+      assert res["signature"] =="##computed_sig##"
     end
 
     test "must put variable length indices into list", ctx do
       res = %FBAgent.Cell{script: ctx.script, params: ["BITCOIN_ECDSA", "1AKHViYgBGbmxi8qiJkNvoHNeDu9m3MfPE", "##computed_sig##", "1", "2", "3", "6", "7"]}
       |> FBAgent.Cell.exec!(ctx.vm)
+      |> Map.get("_AIP")
+      |> List.first
 
-      assert res["_AIP"]["indices"] == ["1", "2", "3", "6", "7"]
+      assert res["indices"] == [1, 2, 3, 6, 7]
+    end
+  end
+
+
+  describe "example from AIP docs with implicit indices" do
+    setup do
+      tx = File.read!("test/mocks/bitcom_aip_get_tape_1.json")
+      |> Jason.decode!
+      |> FBAgent.Adapter.Bob.to_bpu
+      |> List.first
+      %{
+        tx: tx,
+        tape: FBAgent.Tape.from_bpu(tx)
+      }
+    end
+
+    test "must verify the signature", ctx do
+      cell = ctx.tape.cells
+      |> Enum.at(1)
+      |> Map.put(:script, ctx.script)
+
+      vm = ctx.vm
+      |> FBAgent.VM.set!("ctx.tx", ctx.tape.tx)
+      |> FBAgent.VM.set!("ctx.tape_index", ctx.tape.index)
+
+      res = FBAgent.Cell.exec!(cell, vm)
+      |> Map.get("_AIP")
+      |> List.first
+
+      assert res["verified"] == true
+      assert res["indices"] == []
+    end
+  end
+
+
+  describe "example from AIP docs with explicit indices" do
+    setup do
+      tx = File.read!("test/mocks/bitcom_aip_get_tape_2.json")
+      |> Jason.decode!
+      |> FBAgent.Adapter.Bob.to_bpu
+      |> List.first
+      %{
+        tx: tx,
+        tape: FBAgent.Tape.from_bpu(tx)
+      }
+    end
+
+    test "must verify the signature", ctx do
+      cell = ctx.tape.cells
+      |> Enum.at(2)
+      |> Map.put(:script, ctx.script)
+
+      vm = ctx.vm
+      |> FBAgent.VM.set!("ctx.tx", ctx.tape.tx)
+      |> FBAgent.VM.set!("ctx.tape_index", ctx.tape.index)
+
+      res = FBAgent.Cell.exec!(cell, vm)
+      |> Map.get("_AIP")
+      |> List.first
+
+      assert res["verified"] == true
+      assert res["indices"] == Enum.to_list(0..18)
     end
   end
 
